a9f507b390ae01f62609d63d5f9ce817544b3d46
NicoCalvino <nicolascalvino@hotmail.com>
2022-05-13 14:56:56 -0300

    Agregado el app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const { get } = require("http")
+const app = express()
+const path = require("path")
+const PORT = process.env.PORT || 3030
+
+app.use(express.static("public"))
+app.listen(PORT, function(){console.log(`Mercado Liebre corriendo el puer ${PORT}`)})
+
+app.get("/", function(requ,res){
+    res.sendfile(path.join(__dirname,"views","home.html"))
+})
+

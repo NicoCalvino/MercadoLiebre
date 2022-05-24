3a87a9299f29a04ba599ea8c2d6ccf5024fe1011
NicoCalvino <nicolascalvino@hotmail.com>
2022-05-23 21:18:38 -0300

    Mercado Liebre Final

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const PORT = process.env.PORT || 3030
 app.use(express.static("public"))
 app.listen(PORT, function(){console.log(`Mercado Liebre corriendo el puerto ${PORT}`)})
 
-app.get("/", function(requ,res){
-    res.sendfile(path.join(__dirname,"views","home.html"))
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"views","home.html"))
 })
 
+app.get("/register", function(req,res){
+    res.sendFile(path.join(__dirname,"views","register.html"))
+})
+
+app.get("/login", function(req,res){
+    res.sendFile(path.join(__dirname,"views","login.html"))
+})
